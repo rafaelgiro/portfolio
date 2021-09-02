affad89369bbb935df213af68e3ebc64e47644bd
Rafael Giro <me@rafaelgiro.dev>
2021-09-01 21:05:42 -0300

    feat: add blog section title on homepage

--- modules/BlogPreview/BlogPreview.tsx
+++ modules/BlogPreview/BlogPreview.tsx
@@ -1,3 +1,20 @@
+import clsx from "clsx";
+import { useContext } from "react";
+import { Typography } from "../../components/Typography";
+import { ThemeContext } from "../Layout/context/ThemeContext";
+
+import styles from "./styles/BlogPreview.module.scss";
+
 export const BlogPreview = () => {
-  return <div>a</div>;
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div className={styles["blog-preview"]}>
+      <div className={clsx(styles.title, styles[theme])}>
+        <Typography component="h2" typeScale="title-1">
+          BLOG
+        </Typography>
+      </div>
+    </div>
+  );
 };
